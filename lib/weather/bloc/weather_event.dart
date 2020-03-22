72ba19245ad19c5713065d3d193878c59566520b
Billy Huang <10646003@ntub.edu.tw>
2020-03-23 01:26:09 +0800

    feat: Business Logic (Bloc)

---
+++ lib/weather/bloc/weather_event.dart
@@ -0,0 +1,18 @@
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+abstract class WeatherEvent extends Equatable {
+  const WeatherEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class FetchWeather extends WeatherEvent {
+  final String city;
+
+  const FetchWeather({@required this.city}) : assert(city != null);
+
+  @override
+  List<Object> get props => [city];
+}

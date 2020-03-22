4e4d17379dd95e5759949418fba04f18843485ef
Billy Huang <10646003@ntub.edu.tw>
2020-03-23 00:22:47 +0800

    feat: Login Bloc

---
+++ lib/login/bloc/login_bloc.dart
@@ -0,0 +1,40 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+import '../../auth/bloc/bloc.dart';
+import '../../auth/user_repository.dart';
+import 'login_event.dart';
+import 'login_state.dart';
+
+class LoginBloc extends Bloc<LoginEvent, LoginState> {
+  final UserRepository userRepository;
+  final AuthBloc authBloc;
+
+  LoginBloc({
+    @required this.userRepository,
+    @required this.authBloc,
+  })  : assert(userRepository != null),
+        assert(authBloc != null);
+
+  @override
+  LoginState get initialState => LoginInitial();
+
+  @override
+  Stream<LoginState> mapEventToState(LoginEvent event) async* {
+    if (event is LoginButtonPressed) {
+      yield LoginLoading();
+
+      try {
+        final token = await userRepository.authenticate(
+          username: event.username,
+          password: event.password,
+        );
+
+        authBloc.add(LoggedIn(token: token));
+        yield LoginInitial();
+      } catch (error) {
+        yield LoginFailure(error: error.toString());
+      }
+    }
+  }
+}
